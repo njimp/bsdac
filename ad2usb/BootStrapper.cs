dd5ca9132a8b39118360bd67c20142f570121069
njimp <107855571+njimp@users.noreply.github.com>
2023-08-16 17:10:21 +0800

    Add files via upload

---
+++ ad2usb/BootStrapper.cs
@@ -0,0 +1,26 @@
+﻿using Caliburn.Micro;
+using sam2usb.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace sam2usb
+{
+    internal class BootStrapper : BootstrapperBase
+    {
+        public BootStrapper()
+        {
+            this.Initialize();
+            
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            base.OnStartup(sender, e);
+            DisplayRootViewForAsync<ShellViewModel>();
+        }
+    }
+}
